032927cd41221ab5e12b4d2477b8105c4c6e3c0e
thervieu <thibault99hervieu@gmail.com>
2020-05-18 15:59:34 +0200

    added ctrl \ and corrected double ctrl l

--- srcs/termcap/ctrl_stuff.c
+++ srcs/termcap/ctrl_stuff.c
@@ -6,7 +6,7 @@
 /*   By: excalibur <excalibur@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 12:55:44 by thervieu          #+#    #+#             */
-/*   Updated: 2020/05/02 11:20:45 by excalibur        ###   ########.fr       */
+/*   Updated: 2020/05/18 15:58:29 by thervieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 void	clear_screen_(t_line *line)
 {
 	(void)ms;
-	tputs(tgoto(tgetstr("SF", NULL), 0, line->start.row - 1), 1, &tc_putchar);
+	if (line->start.row > 1)
+		tputs(tgoto(tgetstr("SF", NULL), 0, line->start.row - 1), 1, &tc_putchar);
 	line->start.row = 1;
 	set_curpos(line);
 }
